18b46314d4b350b25798d91ac10ed29ccbfe8f2e
bewflast <68069182+bewflast@users.noreply.github.com>
2024-02-18 12:46:56 +0300

    init

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <GarrysMod/Lua/Interface.h>
+
+GMOD_MODULE_OPEN() // NOLINT(*-unused-parameters, *-identifier-length, *-use-trailing-return-type)
+{
+	return 0;
+}
+
+GMOD_MODULE_CLOSE() // NOLINT(*-unused-parameters, *-identifier-length, *-use-trailing-return-type)
+{
+	return 0;
+}
